8e8bd728ded3d15e2f906975dce32c88101e0eb0
Redth <jondick@gmail.com>
2017-03-21 10:34:17 -0400

    Update dependencies

--- Cake.ExtendedNuGet.Tests/Fakes/FakeCakeContext.cs
+++ Cake.ExtendedNuGet.Tests/Fakes/FakeCakeContext.cs
@@ -2,6 +2,7 @@
 using Cake.Core.IO;
 using Cake.Core;
 using System.Collections.Generic;
+using Cake.Core.Tooling;
 
 namespace Cake.Xamarin.Tests.Fakes
 {
@@ -17,15 +18,18 @@ namespace Cake.Xamarin.Tests.Fakes
                 System.IO.Path.GetFullPath (AppDomain.CurrentDomain.BaseDirectory));
             
             var fileSystem = new FileSystem ();
-            var environment = new CakeEnvironment ();
+			var environment = new Cake.Testing.FakeEnvironment(PlatformFamily.Windows);
             var globber = new Globber (fileSystem, environment);
             log = new FakeLog ();
             var args = new FakeCakeArguments ();
             var processRunner = new ProcessRunner (environment, log);
             var registry = new WindowsRegistry ();
-
-            context = new CakeContext (fileSystem, environment, globber, log, args, processRunner, registry);
-            context.Environment.WorkingDirectory = testsDir;
+			var toolRepo = new ToolRepository(environment);
+			var config = new Core.Configuration.CakeConfigurationProvider(fileSystem, environment).CreateConfiguration(testsDir, new Dictionary<string, string>());
+			var toolResolutionStrategy = new ToolResolutionStrategy(fileSystem, environment, globber, config);
+			var toolLocator = new ToolLocator(environment, toolRepo, toolResolutionStrategy);
+			context = new CakeContext(fileSystem, environment, globber, log, args, processRunner, registry, toolLocator);
+			context.Environment.WorkingDirectory = testsDir;
         }
 
         public DirectoryPath WorkingDirectory {
